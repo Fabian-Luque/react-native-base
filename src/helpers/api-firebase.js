f50ce8189291bc890d4e20cdfb1a0bd8dd91296b
Fabian-Luque <fabianluque2014@gmail.com>
2023-09-26 08:10:50 -0300

    first commit

---
+++ src/helpers/api-firebase.js
@@ -0,0 +1,64 @@
+import {
+    addDoc,
+    collection,
+    deleteDoc,
+    doc,
+    getDocs,
+    onSnapshot,
+    orderBy,
+    query,
+    updateDoc,
+} from "firebase/firestore";
+import { FirebaseApp } from "utils/firebase";
+
+export class NoteApi {
+    static async create(note) {
+        const response = await addDoc(
+            collection(FirebaseApp.db, "notes"),
+            note
+        );
+        return {
+            id: response.id,
+            ...note,
+        };
+    }
+
+    static async fetchAll() {
+        const q = query(
+            collection(FirebaseApp.db, "notes"),
+            orderBy("created_at", "asc")
+        );
+        const response = await getDocs(q);
+        return response.docs.map((document) => {
+            return {
+                id: document.id,
+                ...document.data(),
+            };
+        });
+    }
+
+    static async deleteById(noteId) {
+        deleteDoc(doc(FirebaseApp.db, "notes", noteId));
+    }
+
+    static async updateById(id, note) {
+        const q = doc(FirebaseApp.db, "notes", id);
+        await updateDoc(q, note);
+        return {
+            id,
+            ...note,
+        };
+    }
+
+    static onShouldSyncNotes(onChange) {
+        const q = query(collection(FirebaseApp.db, "notes"));
+        const unsub = onSnapshot(q, (querySnapshot) => {
+            const isUserPerformingChange =
+                querySnapshot.metadata.hasPendingWrites;
+            if (!isUserPerformingChange) {
+                onChange();
+            }
+        });
+        return unsub;
+    }
+}
---
+++ src/helpers/api-rest.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3200/notes";
+
+export class NoteApi {
+    static async create(note) {
+        return this.formatId((await axios.post(`${BASE_URL}`, note)).data);
+    }
+
+    static async fetchAll() {
+        return (await axios.get(`${BASE_URL}`)).data.map(this.formatId);
+    }
+
+    static async deleteById(noteId) {
+        return (await axios.delete(`${BASE_URL}/${noteId}`)).data;
+    }
+
+    static async updateById(id, note) {
+        return this.formatId(
+            await axios.patch(`${BASE_URL}/${id}`, note)
+        ).data();
+    }
+
+    static formatId(note) {
+        return {
+            ...note,
+            id: note.id.toString(),
+        };
+    }
+}
