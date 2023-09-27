ee955a7ee8334f89b185dc5b721030593a2ff0c2
Fabian-Luque <fabianluque2014@gmail.com>
2023-09-27 08:30:44 -0300

    tenmplate

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { DataProvider } from "./src/hooks";
+import AppNavigation from "./src/navigation/App";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <DataProvider>
+            <AppNavigation />
+        </DataProvider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
